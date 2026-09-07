e478b291ca7c5be28d715224944c24cd79b881c9
Yura Kaptar <mrnarrow17@gmail.com>
2026-09-07 22:59:26 +0300

    Simplified the health check

--- app/main.py
+++ app/main.py
@@ -8,12 +8,12 @@ from fastapi.responses import JSONResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from sqladmin import Admin
-from sqlmodel import Session, select
+from sqlmodel import Session
 from starlette.middleware.sessions import SessionMiddleware
 
 from app.admin import AdminAuth, ClassAdmin, StaffAdmin
 from app.config import get_settings
-from app.database import engine, get_redis_client, get_session
+from app.database import engine, get_session
 from app.logger import get_app_logger
 from app.middleware import HSTSMiddleware, LoggingMiddleware
 from app.routers import classes, staff
@@ -112,22 +112,8 @@ def root_head():
 
 
 @app.get("/health")
-async def health(db_session: Session = Depends(get_session)):
-    """
-    Returns a 200 OK response if the Redis and database are available, otherwise returns a 503 Service Unavailable response.
-    """
-    redis_ok = True
-    try:
-        await get_redis_client().ping()
-    except Exception:
-        redis_ok = False
-    db_ok = True
-    try:
-        db_session.exec(select(1))
-    except Exception:
-        db_ok = False
-    status_code = 200 if redis_ok and db_ok else 503
-    return JSONResponse({"redis": redis_ok, "db": db_ok}, status_code=status_code)
+def health_check(session: Session = Depends(get_session)):
+    return {"status": "ok"}
 
 
 if __name__ == "__main__":
